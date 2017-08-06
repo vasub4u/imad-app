23c53ef2b771182ffd96cca2789e1e933007ce46
Srinivas Adapa <srinivas2804@gmail.com>
2017-08-06 11:51:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,5 +23,5 @@ app.get('/ui/madi.png', function (req, res) {
 
 var port = 80;
 app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+  console.log(`IMAD course app from Srinivas Adapa listening on port ${port}!`);
 });
